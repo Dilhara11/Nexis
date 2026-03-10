ba633a5d438c1b1266f7e7c90bb803a788bbe14f
A.P.A.K.Dilhara <achinthakaveen11@gmail.com>
2026-03-10 11:02:30 +0530

    feat(): added evaluation and inference scripts

---
+++ inference/detect_image.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import cv2
+
+# load trained model
+model = YOLO("runs/detect/train5/weights/best.pt")
+
+# test image
+image_path = "test.jpg"
+
+results = model(image_path)
+
+# show detection
+for r in results:
+    frame = r.plot()
+    cv2.imshow("Mask Detection", frame)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
